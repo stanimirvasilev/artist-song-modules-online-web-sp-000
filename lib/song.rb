ad240d9ed7bbb9bdf616895d4a5eff26f48484a2
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-06-27 11:15:50 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,12 @@
+
+
 require 'pry'
 
+
 class Song
+  
+  extend Memorable
+  
   attr_accessor :name
   attr_reader :artist
 
@@ -18,13 +24,7 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
+ 
 
   def artist=(artist)
     @artist = artist
